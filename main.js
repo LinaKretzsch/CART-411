8e72ed600c598ec71af3e82917ad9e42c6c87915
LinaKretzsch <lina.kretzschmar@hotmail.de>
2021-02-03 20:48:36 -0500

    Trying new example
    Trying new example

--- main.js
+++ main.js
@@ -1,40 +1,13 @@
+import React from "react";
+import { Canvas} from "react-three-fiber";
+import "./styles.css";
 
 
 
-let scene, camera, renderer;
-
-
-
-function init(){
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xdddddd);
-
-  camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
-
-  hlight = new Three.AmbientLight (0x404040, 100);
-
-  scene.add(hlight);
-
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-}
-
-
-
-let loader = new THREE.GLTFLoader();
-// loader.load('visuali.gltf', function(gltf){
-//   scene.add(gltf.scene);
-//   renderer.render(scene, camera);
-// });
-
-init();
-
-// let controls = new THREE.OrbitControls(camera);
-// controls.addEventListener('change', renderer);
-
-function animate() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
+export default function App() {
+  return (
+    <Canvas>
+      <directionalLight intensity={0.5} />
+    </Canvas>
+  );
 }
